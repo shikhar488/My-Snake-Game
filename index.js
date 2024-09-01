c8d118ef05a7505e0fb0c744325253f33ea265dd
Shikhar Joshi <46199723+shikhar488@users.noreply.github.com>
2024-09-01 14:09:56 +0530

    added codes

--- index.js
+++ index.js
@@ -7,6 +7,48 @@ document.addEventListener('DOMContentLoaded',function(){
     let food = {x:300,y:200};
     let snake = [{x:160,y:200},{x:140,y:200},{x:120,y:200}];
 
+    let dx=cellSize;
+    let dy=0;
+
+    let gameSpeed=200;
+    let intervalId;
+
+    function updateSnake(){
+        const newHead = {x:snake[0].x+dx , y:snake[0].y+dy};
+        snake.unshift(newHead);
+        if(newHead.x==food.x && newHead.y==food.y){
+            score+=10;
+            if(gameSpeed>50){
+                clearInterval(intervalId);
+                gameSpeed-=10;
+                gameLoop();
+            }
+        }
+        else{
+            snake.pop();
+        }
+    }
+
+    function changeDirection(e){
+        const isGoingDown = dy === cellSize;
+        const isGoingUp = dy === -cellSize;
+        const isGoingRight = dx === cellSize;
+        const isGoingLeft = dx === -cellSize;
+        if(e.key === 'ArrowUp' && !isGoingDown ) {
+            dx = 0;
+            dy = -cellSize;
+        } else if(e.key === 'ArrowDown' && !isGoingUp) {
+            dx = 0;
+            dy = cellSize;
+        } else if(e.key === 'ArrowLeft' && !isGoingRight) {
+            dx = -cellSize;
+            dy = 0;
+        } else if(e.key === 'ArrowRight' && !isGoingLeft) {
+            dx = cellSize;
+            dy = 0;
+        }
+    }
+
     function drawDiv(x,y,className){
         const divElement = document.createElement('div');
         divElement.classList.add(className);
@@ -28,11 +70,18 @@ document.addEventListener('DOMContentLoaded',function(){
     
     }
 
+    function gameLoop(){
+        intervalId = setInterval(()=>{
+            updateSnake();
+            drawFoodAndSnake();
+        },gameSpeed);
+    }
+
     function runGame(){
         if(!gameStarted){
             gameStarted=true;
-            drawFoodAndSnake();
-            // gameLoop();
+            document.addEventListener('keydown',changeDirection);
+             gameLoop();
         }
     }
 
